c273a1bc93c731d0e6ba6a14ed619b7eb67b19fd
Dean Taylor <dean@gohitech.net>
2021-09-16 11:12:34 +0800

    fix umask settings in kubectl configuration user session script add default base kubectl config file if not present to standardise configuration presedence settings

--- files/KUBECONFIG.sh
+++ files/KUBECONFIG.sh
@@ -2,14 +2,27 @@ DIR="${HOME}/.kube"
 
 # if kubectl is in PATH / installed
 if which kubectl &>/dev/null; then
-  [ -d "${DIR}" ] || mkdir -p "${DIR}"
+  [ -d "${DIR}" ] || (umask 0077 && mkdir -p "${DIR}")
+
+  # Add generic config file
+  if [[ ! -f ${DIR}/config ]]; then
+    (umask 0077 && cat <<EOT >${DIR}/config
+apiVersion: v1
+clusters: []
+contexts: []
+current-context: ""
+kind: Config
+preferences: {}
+users: []
+EOT
+    )
+  fi
 
   # Configure kubectl for local microk8s
   if ! [ -f "${DIR}/microk8s.config" ]; then
     # if microk8s is in PATH / installed
     if which microk8s &>/dev/null; then
-      umask 0077
-      microk8s config | sed 's/\(user\|name\): admin/\1: microk8s-admin/' > "${DIR}/microk8s.config"
+	    (umask 0077 && microk8s config | sed 's/\(user\|name\): admin/\1: microk8s-admin/' > "${DIR}/microk8s.config")
     fi
   fi
 
